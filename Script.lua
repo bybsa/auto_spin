2b9c36d77d0a7353f463588272e7082de6334671
bybsa <154340017+bybsa@users.noreply.github.com>
2024-02-12 16:58:05 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,12 +1,14 @@
-getgenv().click = true
-
 local plr = game.Players.LocalPlayer
 local gui = plr.PlayerGui.ScreenGui.Wheel
-while wait() do
-    if not click then break end
+
+while true do
+    if not getgenv().click then break end
+    
     for _,v in pairs(gui:GetChildren()) do
         if v:IsA("TextButton") and v.Visible == true then
-            firesignal(v.MouseButton1Click)
+            v.MouseButton1Click:Fire()
         end
     end
+    
+    wait()
 end
