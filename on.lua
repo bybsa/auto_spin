f1d5ab76b296f89cab749b24d227f1787544dc2a
bybsa <154340017+bybsa@users.noreply.github.com>
2024-02-12 16:47:44 +0300

    Update on.lua

--- on.lua
+++ on.lua
@@ -1,6 +1,6 @@
 spawn(function()
     while true do
-        loadstring(game:httpget("https://raw.githubusercontent.com/bybsa/auto_spin/main/script.lua", true))()
+        loadstring(game:httpget("https://raw.githubusercontent.com/bybsa/auto_spin/main/Script.lua", true))()
         wait(183)
     end
 end)
